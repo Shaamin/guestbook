822fd39a3dadabe02ceb2af2adc4132750bf3a60
potupchik <potupchik@sveak.com>
2021-05-03 22:18:53 +0700

    Шаг 25: Уведомление различными способами

--- Makefile
+++ Makefile
@@ -4,3 +4,12 @@ tests:
 	symfony console doctrine:fixtures:load -n
 	symfony php bin/phpunit
 .PHONY: tests
+
+start:
+	docker-compose up -d
+	symfony server:start -d
+	symfony run -d --watch=config,src,templates,vendor symfony console messenger:consume async
+
+stop:
+	docker-compose down
+	symfony server:stop
--- src/Command/CommentCleanupCommand.php
+++ src/Command/CommentCleanupCommand.php
@@ -2,24 +2,28 @@
 
 namespace App\Command;
 
+use App\Notification\CommentReviewNotification;
 use App\Repository\CommentRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Notifier\NotifierInterface;
 
 class CommentCleanupCommand extends Command
 {
     private CommentRepository $commentRepository;
 
     protected static $defaultName = 'app:comment:cleanup';
+    private NotifierInterface $notifier;
 
-    public function __construct(CommentRepository $commentRepository)
+    public function __construct(CommentRepository $commentRepository, NotifierInterface $notifier)
     {
         $this->commentRepository = $commentRepository;
 
         parent::__construct();
+        $this->notifier = $notifier;
     }
 
     protected function configure(): void
@@ -32,18 +36,20 @@ class CommentCleanupCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-
-        if ($input->getOption('dry-run')) {
-            $io->note('Dry mode enabled');
-
-            $count = $this->commentRepository->countOldRejected();
-        } else {
-            $count = $this->commentRepository->deleteOldRejected();
-        }
-
-        $io->success(sprintf('Deleted "%d" old rejected/spam comments.', $count));
-
+        $comment = $this->commentRepository->find(3);
+        $this->notifier->send(new CommentReviewNotification($comment), ...$this->notifier->getAdminRecipients());
+//        $io = new SymfonyStyle($input, $output);
+//
+//        if ($input->getOption('dry-run')) {
+//            $io->note('Dry mode enabled');
+//
+//            $count = $this->commentRepository->countOldRejected();
+//        } else {
+//            $count = $this->commentRepository->deleteOldRejected();
+//        }
+//
+//        $io->success(sprintf('Deleted "%d" old rejected/spam comments.', $count));
+//
         return 0;
     }
 }
